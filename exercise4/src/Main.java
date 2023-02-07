0cf333aa40f7080738cd36d1b2f28b1e4c12c984
sergeemmanuelk <serge_em@outlook.fr>
2023-02-07 08:08:48 +0000

    Update Main file

--- exercise4/src/Main.java
+++ exercise4/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Laptop laptop = new Laptop("Lenovo", "Core i5", 8, 14);
+        laptop.printSpecs();
+        laptop.start();
+        laptop.closeLid();
+
+        System.out.println("***************");
+
+        SmartPhone phone = new SmartPhone("Iphone 13", "A15 Bionic chip" , 8, "iOS");
+        phone.printSpecs();
+        phone.makeCall();
     }
 }
